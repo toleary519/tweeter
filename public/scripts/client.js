4dd0731f17e1051437406d8a0ec2896745756537
vagrant <t.oleary@me.com>
2021-08-12 20:32:55 +0000

    fix fetchPosts with then, add enter as a submit action

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,32 +9,30 @@
 
 $(document).ready(()=> {
   
-  // const fetchPosts = () => {
+  const fetchPosts = () => {
     $.ajax({
       url: '/tweets',
       method: 'GET',
       dataType: 'json',
       success: (post) => {
-        console.log(post);
+        console.log("this is the post info", post);
         renderTweets(post);
       }, 
       error: (err) => {
         console.log(err);
       }
     })
-  // };
+  };
 
   const $form = $('#new-tweet-form');
     $form.on('submit', function (event) {
       event.preventDefault();
 
       const $serializedData = $(this).serialize();
-      console.log($serializedData);
+      console.log("this is the serial tweet:", $serializedData);
 
-      // $.post('/tweets', $serializedData).then(() => {})
-      $.post('/tweets', $serializedData), (response) => {
-        console.log(response);
-      }
+      $.post('/tweets', $serializedData).then(fetchPosts); 
+    
     })
 
   const createTweetElement = (data) => {
@@ -42,12 +40,12 @@ $(document).ready(()=> {
     const $tweet = $(`
       <div class="tweet" >
         <div class="avatar-name"> 
-          <i name ="avatar" src=${data.user.avatars}></i>
+          <img name ="avatar" src=${data.user.avatars}></img>
           <p class="author">${data.user.name}</p>
           <p class="handle">${data.user.handle}</p>
         </div>
         <div class="stored-tweet">
-          <p>${data.content.text}</p>
+          <p>${data.content.text}</input>
         </div>
         <div class="icons-date">
           <time class="timeago">${data.created_at}</time>
@@ -63,7 +61,7 @@ $(document).ready(()=> {
  
   const renderTweets = function(tweets) {
     const $tweetContainer = $('.tweet-container')
-    // $tweetContainer.empty();
+    $tweetContainer.empty();
 
     for (tweet of tweets) {
       const $tweet = createTweetElement(tweet);
